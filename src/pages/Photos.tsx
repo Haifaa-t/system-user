a2b2fa08db59492de02a177eadc9077009d69448
Haifaa-t <h.aloraini-t@tamkeentech.sa>
2025-04-24 15:20:28 +0300

    Update Photos.tsx

--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -1,30 +1,34 @@
 import { useEffect, useState } from 'react';
 import { useSearchParams, Link } from 'react-router-dom';
-import axios from 'axios';
-import PhotoCard from '@/components/PhotoCard';
+import { getPhotosByAlbumId } from '../api/services/photoService';
+import PhotoCard from '../components/PhotoCard';
 import { ArrowLeft } from 'lucide-react';
 
 
-type Photo = {
-  id: number;
-  title: string;
-  thumbnailUrl: string;
-};
-
 
 const Photos = () => {
-  const [photos, setPhotos] = useState<Photo[]>([]);
   const [searchParams] = useSearchParams();
   const albumId = searchParams.get('albumId');
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    if (albumId) {
-      axios.get(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
-        .then((res) => setPhotos(res.data));
-    }
+    const fetchPhotos = async () => {
+      if (albumId) {
+        setLoading(true);
+        const result = await getPhotosByAlbumId(Number(albumId));
+        setPhotos(result.data);
+        setError(result.error);
+        setLoading(result.loading);
+      }
+    };
+
+    fetchPhotos();
   }, [albumId]);
 
-  if (!photos.length) return <div className="text-center py-20">Loading photos...</div>;
+  if (loading) return <div className="text-center py-20">Loading photos...</div>;
+  if (error) return <div className="text-center py-20 text-red-600">{error}</div>;
 
   return (
     <div className="min-h-screen bg-gray-100 py-10 px-4">
@@ -32,12 +36,14 @@ const Photos = () => {
         <h2 className="text-2xl font-bold text-[#d90f1c]">Album Photos</h2>
         <p className="text-gray-700">All photos in this album</p>
       </div>
+
       <div className="max-w-5xl mx-auto">
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {photos.map((photo) => (
             <PhotoCard key={photo.id} title={photo.title} thumbnailUrl={photo.thumbnailUrl} />
           ))}
         </div>
+
         <div className="mt-10 flex justify-end">
           <Link to="/users" className="inline-flex items-center bg-gray-800 text-white py-2 px-4 rounded-lg hover:bg-gray-700 transition">
             <ArrowLeft className="w-4 h-4 mr-2" />
