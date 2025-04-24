de95b33f3f3ecf52415ba3062749bc6ce824830d
Haifaa-t <h.aloraini-t@tamkeentech.sa>
2025-04-24 15:16:00 +0300

    Update UserCard.tsx

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,47 +1,37 @@
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Mail, Phone, MapPin, Building2 } from 'lucide-react';
+import { User } from '../api/services/userService';
 
 type Props = {
-  user: {
-    id: number;
-    name: string;
-    email: string;
-    phone: string;
-    address: { city: string };
-    company: { name: string };
-  };
+  user: User;
+  color: string;
 };
 
-const UserCard: FC<Props> = ({ user }) => {
+
+
+const UserCard: FC<Props> = ({ user, color }) => {
   const navigate = useNavigate();
 
   return (
     <div
       onClick={() => navigate(`/users/${user.id}`)}
-      className="bg-white rounded-xl shadow-sm p-6 hover:shadow-2xl transition duration-300 border-t-4 border-[#d90f1c] cursor-pointer"
+      className={`bg-white rounded-xl shadow-md p-6 cursor-pointer transform transition hover:scale-105 border-t-8 ${color}`}
     >
-      <h2 className="text-xl font-bold text-[#d90f1c] mb-3">{user.name}</h2>
-      <div className="text-gray-700 space-y-2 text-sm">
-        <div className="flex items-center gap-2">
-          <Mail className="w-4 h-4 text-gray-500" />
-          <p>{user.email}</p>
+      <div className="flex items-center mb-4">
+      
+        <div className="ml-4">
+          <h2 className="text-xl font-bold text-[#d90f1c]">{user.name}</h2>
         </div>
-        <div className="flex items-center gap-2">
-          <Phone className="w-4 h-4 text-gray-500" />
-          <p>{user.phone}</p>
-        </div>
-        <div className="flex items-center gap-2">
-          <MapPin className="w-4 h-4 text-gray-500" />
-          <p>{user.address.city}</p>
-        </div>
-        <div className="flex items-center gap-2">
-          <Building2 className="w-4 h-4 text-gray-500" />
-          <p>{user.company.name}</p>
-        </div>
+      </div>
+      <div className="space-y-1 text-sm text-gray-700">
+        <Phone className="inline w-4 h-4 mr-1" /> {user.phone}
+        <p><MapPin className="inline w-4 h-4 mr-1" /> {user.address.city}</p>
+        <p><Mail className="inline w-4 h-4 mr-1" /> {user.email}</p>
       </div>
     </div>
   );
 };
 
 export default UserCard;
+
