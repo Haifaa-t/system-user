98986ad55035241b312c43bdce48044eb3e0d68b
Haifaa-t <h.aloraini-t@tamkeentech.sa>
2025-04-24 15:17:03 +0300

    Update PhotoCard.tsx

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -1,4 +1,3 @@
-
 type Props = {
   title: string;
   thumbnailUrl: string;
@@ -6,15 +5,13 @@ type Props = {
 
 const PhotoCard = ({ title, thumbnailUrl }: Props) => {
   return (
-    <div className="bg-white shadow-md rounded-lg overflow-hidden">
+    <div className="bg-white p-4 rounded-lg shadow-md text-center">
       <img
         src={thumbnailUrl}
         alt={title}
-        className="w-full h-48 object-cover"
+        className="w-full h-40 object-cover rounded mb-3"
       />
-      <div className="p-4">
-        <h2 className="text-lg font-semibold text-gray-800">{title}</h2>
-      </div>
+      <p className="text-sm text-gray-700">{title}</p>
     </div>
   );
 };
